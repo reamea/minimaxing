e6f0a82012b36a2c6da87e5dce30c21768551f35
reamea <cheyreamea7@gmail.com>
2023-11-20 11:53:16 +0700

    Updated files

--- script.sh
+++ script.sh
@@ -1,3 +1,21 @@
+# Download Website
+aria2c https://html5up.net/minimaxing/download
+unzip html5up-minimaxing.zip && rm html5up-minimaxing.zip
+
+# Create docker file
+cat <<% > Dockerfile
+FROM debian
+
+RUN apt update && apt -y install apache2
+WORKDIR /var/www/html
+COPY . ./
+
+EXPOSE 80
+CMD apachectl -D FOREGROUND
+%
+
+# update Readme
+cat <<% > README.md
 # Minimaxing
 This is public repo for minimaxing with docker file to build image from debian apache2 and expose 80.
 
@@ -14,3 +32,10 @@ This is public repo for minimaxing with docker file to build image from debian a
 ## Stop Docker run
     docker stop <CONTAINER ID>
     docker stop 6e72db6640a9
+%
+
+# push to github
+git status
+git add .
+git commit -m "Updated files"
+git push origin main
